051bb66b57b8eb271229be2488d2747905b89700
Septriasa <hsakdiyah059@gmail.com>
2026-05-25 19:12:28 +0700

    db

--- config/database.php
+++ config/database.php
@@ -1,32 +1,26 @@
 <?php
 class Database {
-    // Properti konfigurasi database
     private $host     = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'uniqlouse';
+    private $username = 'hexacode_uniqlouser';
+    private $password = 'rahasiaUniqlouse';
+    private $database = 'hexacode_db_tambahan';
     private $koneksi;
 
-    // Method untuk membuat dan mengambil koneksi
     public function getConnection() {
         $this->koneksi = null;
 
         try {
-            // Membuat koneksi PDO
             $this->koneksi = new PDO(
                 "mysql:host=" . $this->host . ";dbname=" . $this->database . ";charset=utf8mb4", 
                 $this->username, 
                 $this->password
             );
-            
-            // Mengatur mode error
             $this->koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->koneksi->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             
         } catch (PDOException $e) {
             die("Koneksi database gagal: " . $e->getMessage());
         }
-
         return $this->koneksi;
     }
 }
